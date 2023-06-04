782053a99d555ec728e17a143865c2416c5328ff
BeshoyKhater <BeshoyKhater@gmail.com>
2023-06-05 01:33:35 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//Pages
+import { Home, CategoryProduct, ProductSingle, Cart, Search } from "./pages/index";
+//components
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Footer from "./components/Footer/Footer";
+import { Provider } from 'react-redux';
+import store from './store/store';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Header />
+          <Sidebar />
+
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/product/:id' element={<ProductSingle />} />
+            <Route path='/category/:category' element={<CategoryProduct />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/search/:searchTerm' element={<Search />} />
+            <Route path='/search' element={<Search />} />
+          </Routes>
+
+          <Footer />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
---
+++ src/utils/images.js
@@ -0,0 +1,16 @@
+
+import slider_img_1 from "../assets/images/slider1.jpg";
+import slider_img_2 from "../assets/images/slider3.jpg";
+import Loader from "../assets/images/Infinity-1s-200px.svg";
+
+
+const images = [
+    slider_img_1,
+    slider_img_2,
+];
+
+export const loader = [
+    Loader,
+];
+
+export default images;
